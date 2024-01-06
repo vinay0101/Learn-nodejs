3c055d300d612397d200535abab2f848da24c228
Vinay Jain <f20190728@pilani.bits-pilani.ac.in>
2024-01-07 02:56:19 +0530

    Add templating engines

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
-const path = require("path");
+const path = require('path');
 
-const express = require("express");
-const bodyParser = require("body-parser");
-
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.set('view engine', 'pug');
+app.set('views', 'views');
 
-app.use("/admin", adminRoutes);
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-	res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
-app.listen(3000, () => {
-	console.log("Server running on port 3000");
-});
+app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -2,12 +2,14 @@ const path = require('path');
 
 const express = require('express');
 
+const rootDir = require('../util/path');
+const adminData = require('./admin');
+
 const router = express.Router();
 
-const rootDir = require('../utils/path');
-
-router.get("/", (req, res, next) => {
-	res.sendFile(path.join(rootDir, 'views', 'shop.html'))
+router.get('/', (req, res, next) => {
+  console.log('shop.js', adminData.products);
+  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = router;
